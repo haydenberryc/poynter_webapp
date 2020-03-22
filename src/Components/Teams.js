ffa7c3d69140c943f6db07684a713e9a7b213287
haydenberryc <haydenberryc@Haydens-MacBook-Pro.local>
2020-03-22 17:30:47 -0500

    updated team creation ui

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -43,7 +43,7 @@ export default class Teams extends React.Component {
             let points = await getPoints(accessToken);
             let players = [];
             points.forEach((player) => {
-                players.push(<tr key={player[0]} onClick={this.add_selected_player}><td>{player[0]}</td><td>{player[2]}</td></tr>)
+                players.push(<tr key={player[0]} onClick={this.add_selected_player}><td style={{ width: '50%' }}>{player[0]}</td><td style={{ width: '50%' }}>{player[2]}</td></tr>)
             })
 
             // Update the array of players in state
@@ -72,7 +72,7 @@ export default class Teams extends React.Component {
         if (!f_isThere) {
             console.log("need to update state")
             this.setState({
-                selected_players: [...this.state.selected_players, <tr key={name} onClick={this.remove_selected_player}><td>{name}</td><td>{avg}</td></tr>],
+                selected_players: [...this.state.selected_players, <tr key={name} onClick={this.remove_selected_player}><td style={{ width: '50%' }}>{name}</td><td style={{ width: '50%' }}>{avg}</td></tr>],
                 selected_names: [...this.state.selected_names, name]
             }, function () {
                 console.log("state updated... sorting players");
@@ -164,34 +164,34 @@ export default class Teams extends React.Component {
             this.state.team_lobby.forEach(player => {
 
                 if (player.team === 1) {
-                    t_one.push(<tr key={t_one.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_one.push(<tr key={t_one.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 2) {
-                    t_two.push(<tr key={t_two.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_two.push(<tr key={t_two.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 3) {
-                    t_three.push(<tr key={t_three.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_three.push(<tr key={t_three.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 4) {
-                    t_four.push(<tr key={t_four.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_four.push(<tr key={t_four.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 5) {
-                    t_five.push(<tr key={t_five.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_five.push(<tr key={t_five.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 6) {
-                    t_six.push(<tr key={t_six.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_six.push(<tr key={t_six.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 7) {
-                    t_seven.push(<tr key={t_seven.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_seven.push(<tr key={t_seven.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 8) {
-                    t_eight.push(<tr key={t_eight.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_eight.push(<tr key={t_eight.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 9) {
-                    t_nine.push(<tr key={t_nine.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_nine.push(<tr key={t_nine.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
                 else if (player.team === 10) {
-                    t_ten.push(<tr key={t_ten.length}><td>{player.name}</td><td>{player.average}</td></tr>)
+                    t_ten.push(<tr key={t_ten.length}><td style={{ width: '50%' }}>{player.name}</td><td style={{ width: '50%' }}>{player.average}</td></tr>)
                 }
             })
             this.setState({
@@ -265,98 +265,107 @@ export default class Teams extends React.Component {
     render() {
         return (
             <div>
-                <h1>Teams</h1>
-                <div style={{ height: 500 }}>
-                    <div style={{ height: 500, overflowY: 'scroll', width: '50%', float: 'left' }}>
-                        <Table bordered striped style={{}}>
-                            <thead>
-                                <tr><th colSpan='2' style={{ textAlign: 'center', fontSize: 20 }}>ALL PLAYERS</th></tr>
-                                <tr>
-                                    <th style={{ position: 'sticky' }}>PLAYER NAME</th>
-                                    <th style={{ position: 'sticky' }}>AVERAGE SCORE</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.all_players}
-                            </tbody>
-                        </Table>
+                <div style={{ paddingTop: 70, height: 500 }}>
+                    <div style={{ float: 'left', width: '50%' }}>
+                        <div>
+                            <Table bordered striped>
+                                <thead>
+                                    <tr><th colSpan='2' style={{ textAlign: 'center', fontSize: 20 }}>ALL PLAYERS</th></tr>
+                                    <tr>
+                                        <th style={{ width: '50%' }}>PLAYER NAME</th>
+                                        <th style={{ width: '50%' }}>AVERAGE SCORE</th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                        </div>
+                        <div style={{ height: 300, overflowY: 'scroll' }}>
+                            <Table bordered striped>
+                                <tbody>
+                                    {this.state.all_players}
+                                </tbody>
+                            </Table>
+                        </div>
                     </div>
-                    <div style={{ height: 500, overflowY: 'scroll', width: '50%', float: 'left' }}>
-                        <Table bordered striped style={{}}>
-                            <thead style={{ textAlign: 'center', fontSize: 20 }}><tr><th colSpan="2">SELECTED PLAYERS: {this.state.selected_players.length}</th></tr></thead>
-                            <thead>
-                                <tr>
-                                    <th>PLAYER NAME</th>
-                                    <th>AVERAGE SCORE</th>
-                                </tr>
-                            </thead>
-                            <tbody style={{}}>
-                                {this.state.selected_players}
-                            </tbody>
-                        </Table>
+                    <div style={{ float: 'right', width: '50%' }}>
+                        <div>
+                            <Table bordered striped>
+                                <thead>
+                                    <tr><th colSpan='2' style={{ textAlign: 'center', fontSize: 20 }}>TODAY'S PLAYERS</th></tr>
+                                    <tr>
+                                        <th style={{ width: '50%' }}>PLAYER NAME</th>
+                                        <th style={{ width: '50%' }}>AVERAGE SCORE</th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                        </div>
+                        <div style={{ height: 300, overflowY: 'scroll' }}>
+                            <Table bordered striped>
+                                <tbody>
+                                    {this.state.selected_players}
+                                </tbody>
+                            </Table>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <Button onClick={this.increment}>+</Button>
-                        <Button onClick={this.decrement}>-</Button>
-                        <h2>{this.state.numOfTeams}</h2>
-                    </div>
-                    <div>
+                <div style={{ paddingTop: 10 }}>
+                    <div style={{ float: 'left', width: '50%', textAlign: 'center' }}>
                         <form onSubmit={this.handleSubmit}>
-                            <label>
-                                Guest Name:
-                                <input type="text" value={this.state.guest_name} onChange={this.handleNameChange} />
-                            </label>
-                            <label>
-                                Guest Avgerage:
-                                <input type='text' value={this.state.guest_avg} onChange={this.handleAvgChange} />
-                            </label>
-                            <input type="submit" value="Submit" />
+                            <h3>Guest Name</h3>
+                            <input type="text" onChange={this.handleNameChange} value={this.state.guest_name}></input>
+                            <h3>Guest Average</h3>
+                            <input type="text" onChange={this.handleAvgChange} value={this.state.guest_avg}></input>
+                            <h3></h3>
+                            <Button type="submit" onClick={this.handleSubmit}><text>Add Guest</text></Button>
                         </form>
                     </div>
-                    <div>
-                        <Button onClick={this.makeTeamsABCD}>Make Teams ABCD</Button>
+                    <div style={{ float: 'right', width: '50%', textAlign: 'center' }}>
+                        <h3>Today's Players: {this.state.selected_players.length}</h3>
+                        <h3>Teams: {this.state.numOfTeams}</h3>
+                        <Button style={{ margin: 10, width: 50 }} onClick={this.decrement}><text>-</text></Button>
+                        <Button style={{ margin: 10, width: 50 }} onClick={this.increment}><text>+</text></Button>
+                    </div>
+                    <div style={{ clear: 'right', float: 'right', width: '50%', textAlign: 'center' }}>
+                        <Button style={{ margin: 10, width: 200 }} onClick={this.makeTeamsABCD}><text>Make Teams ABCD</text></Button>
                     </div>
                 </div>
                 <div>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 1</th></tr></thead>
                         <tbody>{this.state.team_one}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 2</th></tr></thead>
                         <tbody>{this.state.team_two}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 3</th></tr></thead>
                         <tbody>{this.state.team_three}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 4</th></tr></thead>
                         <tbody>{this.state.team_four}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 5</th></tr></thead>
                         <tbody>{this.state.team_five}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 6</th></tr></thead>
                         <tbody>{this.state.team_six}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 7</th></tr></thead>
                         <tbody>{this.state.team_seven}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 8</th></tr></thead>
                         <tbody>{this.state.team_eight}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 9</th></tr></thead>
                         <tbody>{this.state.team_nine}</tbody>
                     </Table>
-                    <Table>
+                    <Table striped>
                         <thead><tr><th>TEAM 10</th></tr></thead>
                         <tbody>{this.state.team_ten}</tbody>
                     </Table>
